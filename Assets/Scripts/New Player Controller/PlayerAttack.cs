6ba83d40559c10d8e9e58f5a6355d7d8591683e0
AndromedaSnow <andromedasnow@gmail.com>
2024-10-04 19:41:36 -0500

    Player Attacks Consume Ammo

--- Assets/Scripts/New Player Controller/PlayerAttack.cs
+++ Assets/Scripts/New Player Controller/PlayerAttack.cs
@@ -6,7 +6,14 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private int _attackRange;
     [SerializeField] private LayerMask _layerMask;
 
-    [SerializeField] private Vector3 _targetingOffset;
+    [SerializeField] private Vector3 _raycastOffset;
+
+    private PlayerMovementHandler _movementHandler;
+
+    private void Awake()
+    {
+        _movementHandler = GetComponent<PlayerMovementHandler>();
+    }
 
     private void OnEnable()
     {
@@ -20,15 +27,27 @@ public class PlayerAttack : MonoBehaviour
 
     private void FireWeapon(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
-
-        Ray ray = new(transform.position + _targetingOffset, transform.forward);       // Ray forward from player object.
-        RaycastHit hit;
-
-        if(Physics.Raycast(ray, out hit, _attackRange, _layerMask))
+        if(_movementHandler.curMoveState == MoveStates.Aiming)
         {
-            hit.transform.gameObject.GetComponent<IDamageable>()?.ValueChange(_damageAmount);       // Apply damage.
-        }
+            foreach (InventoryItem item in InventoryManager.instance.inventory)                  // Find the appropriate ammo item.
+            {
+                if (item.itemName == "Light Ammo" && item.amount > 0)
+                {
+                    item.amount -= 1;
 
-        Debug.DrawLine(transform.position, ray.GetPoint(_attackRange), Color.magenta, 5f);
+                    Ray ray = new(transform.position + _raycastOffset, transform.forward);       // Ray forward from player object.
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit, _attackRange, _layerMask))
+                    {
+                        hit.transform.gameObject.GetComponent<IDamageable>()?.ValueChange(_damageAmount);       // Apply damage.
+                    }
+
+                    Debug.DrawLine(transform.position, ray.GetPoint(_attackRange), Color.magenta, 5f);          // Debug ray.
+
+                    InventoryManager.instance.refreshInventory();                                               // Refresh inventory UI.
+                }
+            }
+        }
     }
 }
